1847504b9aff5f528f5e81e674e0f2a1a3645e1c
Rehansmit <rehansmit724@gmail.com>
2024-11-15 12:09:28 +0500

    project completed

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 
-
 // functin for Start Game
 
 function startGame() {
 
     var player1Name = document.getElementById("player1").value;
-     var player2Name = document.getElementById("player2").value;
+    var player2Name = document.getElementById("player2").value;
 
     if (player1Name === "") {
         Swal.fire({
@@ -27,52 +26,58 @@ function startGame() {
           });
         return;}    
     
-   
-window.location.href = "game.html";
+   document.getElementById("maindiv").classList.remove("d-none")
+   document.getElementById("pname").classList.add("d-none")
+   var p1n = document.getElementById("player1").value  
+   var p2n = document.getElementById("player2").value  
+
+   document.getElementById("p1name").innerHTML=`<h4>${p1n}</h4>`
+   document.getElementById("p2name").innerHTML=`<h4>${p2n}</h4>`
+
 
         }
 
 
+
         
+
         function clikonhead (){
+          
           document.getElementById("p1slected").innerHTML=`<h4> Head </h4>`
           document.getElementById("p2slected").innerHTML=`<h4> Tail </h4>`
-
+          
         }
 
 
         function clikontail (){
           document.getElementById("p1slected").innerHTML=`<h4> Tail </h4>`
           document.getElementById("p2slected").innerHTML=`<h4> Head </h4>`
-
         }
 
 
 
+
         
-function stillimage (){
-  document.getElementById("flip_img").scr ="assets/head.jpg"
-  var p1slected = document.getElementById("p1slected").value       
-   
-
-}
-
-function tossthecoin(){ 
-stillimage()
- 
   
+function tossthecoin()
+{ 
+document.getElementById("flip_img").scr ="assets/head.jpg"
+var p_selected =document.getElementById("p1slected").innerText
+
+var p1nm = document.getElementById("player1").value  
+var p2nm = document.getElementById("player2").value  
+ 
 coinarr=["Head","Tail"]
 randomvalue= Math.floor(Math.random()*2)
+var p
 var result = coinarr[randomvalue]
 
-console.log(result)
-
-if(result === p1slected){
+if(result=== p_selected){
   
 Swal.fire
 ({
-title: "Congratulations! " + "Player 1", 
-text: "Result is "  + result + " You Won" ,
+title: "Congratulations! " + p1nm, 
+text: "Result is "  + result + " "+"You Won" ,
 icon: "success"
 });
 
@@ -81,16 +86,15 @@ icon: "success"
 else{
     Swal.fire
     ({
-      title: "Congratulations! " + "Player 2 " , 
-      text: "Result is "  + result + " You Won" ,
+      title: "Congratulations! " + p2nm , 
+      text: "Result is "  + result +" "+ "You Won",
       icon: "success"
     });
 
    }
    
-      
-      
-    setTimeout(stillimage,3000)
+        
+    // setTimeout(tossthecoin,5000)
    }
     
 
